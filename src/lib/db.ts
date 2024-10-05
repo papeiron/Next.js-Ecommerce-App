97641d4bf46b29a28316bc8420375d321b6fffe0
papeiron <151983680+papeiron@users.noreply.github.com>
2024-10-05 03:40:31 +0300

    some fix db

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,5 +1,18 @@
 import { PrismaClient } from '@prisma/client';
 
-const db = new PrismaClient();
+declare global {
+  var cachedPrisma: PrismaClient;
+}
 
-export { db };
+let prisma: PrismaClient;
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!global.cachedPrisma) {
+    global.cachedPrisma = new PrismaClient();
+  }
+
+  prisma = global.cachedPrisma;
+}
+
+export const db = prisma;
